178c573f866a1b1a06adeb8f2413c3ad1382841e
Kha <nguyencaokha131995@gmail.com>
2024-04-08 19:59:00 +0200

    change gh actions

--- src/lib.rs
+++ src/lib.rs
@@ -23,8 +23,8 @@ pub fn App() -> impl IntoView {
 
         <Router>
             <Routes>
-                <Route path="/notfound" view=NotFound/>
-                <Route path="/home" view=Home/>
+            <Route path="/" view=Home/>
+            <Route path="/*" view=NotFound/>
             </Routes>
         </Router>
     }
