e4cb7a2225ed7fc5feea1258f793b194cceba0e0
Kha <nguyencaokha131995@gmail.com>
2024-04-29 17:15:53 +0200

    wip pong game

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -5,36 +5,43 @@ use leptos::*;
 #[component]
 pub fn Home() -> impl IntoView {
     // let (count, set_count) = create_signal(0);
-    let ua_info = get_ua().expect("user agent should not be empty");
-    let platform = ua_info.category;
-    let bevy_iframe = move || if platform.to_uppercase() != "PC" { 
+    // let ua_info = get_ua().expect("user agent should not be empty");
+    // let platform = ua_info.category;
+    // let bevy_iframe = move || if platform.to_uppercase() != "PC" { 
+    //     view! {
+    //     <span class="text-base text-slate-700 dark:text-slate-200">
+    //         <p>
+    //             {format!(
+    //                 "You are currently on a \"{}\". Hence, you can't see the gizmo game. 
+    //                 Try to access this page with a laptop to see the gizmo!",
+    //                 platform.to_uppercase(),
+    //             )}
+    //
+    //         </p>
+    //         <p>"This website is made with Leptos, a Rust framework for web apps"</p>
+    //         <br/>
+    //         <span class="flex justify-center">
+    //             <button class="rounded-full bg-slate-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
+    //                 <a href="https://nckportfolio.vercel.app" target="_blank">
+    //                     <span>Migrating from Nuxt 3</span>
+    //                 </a>
+    //             </button>
+    //         </span>
+    //
+    //     </span>
+    //     }
+    // } else {
+    //     view! {
+    //     <span class="text-base text-slate-700 dark:text-slate-200">
+    //         <iframe class="w-[1280px] h-[720px]" id="bevy-game" src="/game/index.html" allow="fullscreen"></iframe>
+    //     </span>
+    //     }
+    //  };
+    let bevy_iframe = move || {
         view! {
-        <span class="text-base text-slate-700 dark:text-slate-200">
-            <p>
-                {format!(
-                    "You are currently on a \"{}\". Hence, you can't see the gizmo game. 
-                    Try to access this page with a laptop to see the gizmo!",
-                    platform.to_uppercase(),
-                )}
-
-            </p>
-            <p>"This website is made with Leptos, a Rust framework for web apps"</p>
-            <br/>
-            <span class="flex justify-center">
-                <button class="rounded-full bg-slate-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
-                    <a href="https://nckportfolio.vercel.app" target="_blank">
-                        <span>Migrating from Nuxt 3</span>
-                    </a>
-                </button>
-            </span>
-
-        </span>
-        }
-    } else {
-        view! {
-        <span class="text-base text-slate-700 dark:text-slate-200">
-            <iframe class="w-[1280px] h-[720px]" id="bevy-game" src="/game/index.html" allow="fullscreen"></iframe>
-        </span>
+            <span class="text-base text-slate-700 dark:text-slate-200">
+                <iframe class="w-[1280px] h-[720px]" id="bevy-game" src="/game/index.html" allow="fullscreen"></iframe>
+                </span>
         }
     };
     view! {
